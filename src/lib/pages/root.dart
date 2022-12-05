aadd2ccba191848081ca44e14538b20b59212623
mohammadammash <mohammadammash31@gmail.com>
2022-12-05 20:49:29 +0200

    src/lib/pages/root.dart: Modifying bottom nav

--- src/lib/pages/root.dart
+++ src/lib/pages/root.dart
@@ -13,23 +13,43 @@ class _RootPageState extends State<RootPage> {
   int currentPage = 0;
   List<Widget> pages = const [HomePage(), SettingsPage()];
 
+  void onTapHandler(int index) {
+    setState(() {
+      currentPage = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("My Wallet")),
-      body: pages[currentPage],
-      bottomNavigationBar: NavigationBar(
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-          NavigationDestination(icon: Icon(Icons.settings), label: 'Settings')
-        ],
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPage = index;
-          });
-        },
-        selectedIndex: currentPage,
-      ),
-    );
+        appBar: AppBar(title: const Text("My Wallet")),
+        body: pages[currentPage],
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: currentPage,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "HOME",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: "SETTINGS",
+              ),
+            ],
+            onTap: (int index) => onTapHandler(index)));
   }
 }
+
+// NavigationBar(
+//         destinations: const [
+//           NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+//           NavigationDestination(icon: Icon(Icons.settings), label: 'Settings')
+//         ],
+//         onDestinationSelected: (int index) {
+//           setState(() {
+//             currentPage = index;
+//           });
+//         },
+//         selectedIndex: currentPage,
+//       ),
