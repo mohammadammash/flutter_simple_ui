2fdde410e47e8058cce8cf69710fc958e90a32bc
mohammadammash <mohammadammash31@gmail.com>
2022-12-05 18:57:22 +0200

    src/lib/pages/add_card.dart: Adding new screen with basic app bar to test stack navigation

---
+++ src/lib/pages/add_card.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AddCardPage extends StatefulWidget {
+  const AddCardPage({super.key});
+
+  @override
+  State<AddCardPage> createState() => _AddCardPageState();
+}
+
+class _AddCardPageState extends State<AddCardPage> {
+  bool isSwitch = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add New Card'),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                debugPrint('Actions');
+              },
+              icon: const Icon(Icons.info_outline))
+        ],
+      ),
+      body: Column(),
+    );
+  }
+}
