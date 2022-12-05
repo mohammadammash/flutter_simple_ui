4e95af48e26beab9d6a743b6629ac447cf785b74
mohammadammash <mohammadammash31@gmail.com>
2022-12-05 16:17:47 +0200

    src/lib/utils/list_tile.dart: Adding recomended constants

--- src/lib/utils/list_tile.dart
+++ src/lib/utils/list_tile.dart
@@ -1,48 +1,61 @@
 import 'package:flutter/material.dart';
 
 class ListTileUtil extends StatelessWidget {
-  const ListTileUtil({super.key});
+  //params
+  final String iconImagePath;
+  final String tileTitle;
+  final String tileSubtitle;
+  //constructor
+  const ListTileUtil({
+    super.key,
+    required this.iconImagePath,
+    required this.tileTitle,
+    required this.tileSubtitle,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          children: [
-            Container(
-              height: 80,
-              decoration: BoxDecoration(
-                color: Colors.grey[100],
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Image.asset('lib/icons/statistics.png'),
-            ),
-            const SizedBox(
-              width: 20,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Statistics',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    )),
-                const SizedBox(
-                  height: 5,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 25.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 80,
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                Text('Payments and Icons',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey[600],
-                    )),
-              ],
-            ),
-          ],
-        ),
-        const Icon(Icons.arrow_forward_ios)
-      ],
+                child: Image.asset(iconImagePath),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(tileTitle,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      )),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(tileSubtitle,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey[600],
+                      )),
+                ],
+              ),
+            ],
+          ),
+          const Icon(Icons.arrow_forward_ios)
+        ],
+      ),
     );
   }
 }
