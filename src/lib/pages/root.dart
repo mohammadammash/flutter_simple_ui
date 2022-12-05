ca9a4f4fb55429d504adcb4566ac537cf1fcf0bd
mohammadammash <mohammadammash31@gmail.com>
2022-12-05 18:36:03 +0200

    src/lib/pages/root.dart: Extracting root page with home and bottom navigator to pages

---
+++ src/lib/pages/root.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:wallet_simple_demo/pages/home.dart';
+import 'package:wallet_simple_demo/pages/settings.dart';
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
+  List<Widget> pages = const [HomePage(), SettingsPage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("My Wallet")),
+      body: pages[currentPage],
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.settings), label: 'Settings')
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        selectedIndex: currentPage,
+      ),
+    );
+  }
+}
