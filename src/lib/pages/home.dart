02bf590b4dfff28ffeea5bb47fd38932be786e59
mohammadammash <mohammadammash31@gmail.com>
2022-12-05 13:42:13 +0200

    src/lib/pages/home.dart: Adding external card and cleaning code

--- src/lib/pages/home.dart
+++ src/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wallet_simple_demo/utils/Wallet_Card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,9 +11,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    //local styling
-    const cardtext_color = Colors.white;
-
     return Scaffold(
         body: SafeArea(
       child: Column(
@@ -42,34 +40,7 @@ class _HomePageState extends State<HomePage> {
           //SECTION SEPERATOR PADDING
           const SizedBox(height: 25),
 
-          //START OF CARDS:
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 30),
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.deepPurple[300],
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Balance',
-                    style: TextStyle(color: cardtext_color, fontSize: 18)),
-                const SizedBox(height: 12),
-                const Text('\$5,250.20',
-                    style: TextStyle(
-                        color: cardtext_color, fontSize: 35, fontWeight: FontWeight.bold)),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text("**** 3456", style: TextStyle(color: cardtext_color)),
-                    Text('10/24', style: TextStyle(color: cardtext_color)),
-                  ],
-                )
-              ],
-            ),
-          )
+          const WalletCard()
         ],
       ),
     ));
