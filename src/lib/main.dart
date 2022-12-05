51271272fca8e4c5d8992ec4f5b91cbe912a12b8
mohammadammash <mohammadammash31@gmail.com>
2022-12-05 18:37:55 +0200

    src/lib/main.dart: Extracting pages from main to root page and use it within home

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wallet_simple_demo/pages/home.dart';
+import 'package:wallet_simple_demo/pages/root.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: const RootPage(),
     );
   }
 }
