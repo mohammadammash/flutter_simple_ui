95b071abf892f09c74f77125469cde79e25dabd7
mohammadammash <mohammadammash31@gmail.com>
2022-12-05 13:07:45 +0200

    src/lib/pages/home.dart: Adding top appBar design

--- src/lib/pages/home.dart
+++ src/lib/pages/home.dart
@@ -10,6 +10,36 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+            //START OF APPBAR CUSTOMIZED
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'My Cards',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.grey,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(Icons.add),
+                )
+              ],
+            ),
+          ),
+
+          //SECTION SEPERATOR PADDING
+          const SizedBox(height: 25),
+        ],
+      ),
+    ));
   }
 }
